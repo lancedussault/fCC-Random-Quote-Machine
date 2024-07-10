848895b5eb1d9606d58170ff4cf8faffec97c664
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-07-10 13:00:27 -0600

    Setting up the project following the tutorial

---
+++ fcc-random-quote-machine/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+
+interface Quote {
+  quote: string;
+  author: string;
+};
+
+const getRandomQuote = (): Quote => {
+  return quotes
+}
+
+function App() {
+  const [quote, setQuote] = useState<Quote>(getRandomQuote());
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
